84c2863c5eb38623db3baf29171c5f4f5881a480
Payroma Developer <payromafinance@gmail.com>
2021-12-22 18:14:55 +0200

    Deployer improvement

--- deploy.py
+++ deploy.py
@@ -5,6 +5,8 @@ import json
 
 
 def run():
+    global abi, bytecode, contract
+
     # Validation
     try:
         contract_name = sys.argv[1]
@@ -32,6 +34,7 @@ def run():
     signed_txn = w3.eth.account.sign_transaction(data, private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    contract = w3.eth.contract(tx_receipt.contractAddress, abi=abi)
 
     # Debugging
     print(f"""
@@ -44,5 +47,7 @@ def run():
     """)
 
 
-if __name__ == '__main__':
-    run()
+abi = None
+bytecode = None
+contract = None
+run()
